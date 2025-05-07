2f969f40628d26302f05683c83a8189fe5b6b0f4
LukitoZ75 <lu.alopess17@gmail.com>
2025-05-06 22:28:30 -0300

    Commit Lucas

---
+++ Assets/Snake.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Direction
+{
+    Up,
+    Right,
+    Down,
+    Left
+}
+
+public class MiniSnake : MonoBehaviour
+{
+    GameManager gameManager;
+    Direction currentDirection = Direction.Right;
+    public GameObject headPrefab;
+    public GameObject tailPrefab;
+    List<GameObject> tail = new List<GameObject>();
+    public float baseSpeed = 5;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        GameObject head = Instantiate(headPrefab, transform.position, Quaternion.identity);
+        tail.Add(head);
+        head.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.gameHasEnded)
+        {
+            return;
+        }
+        HandleRotationInput();
+        MoveForward();
+        UpdateTailPositions();
+    }
+    void RotateCounterClockwise()
+    {
+        currentDirection = (Direction)(((int)currentDirection + 3) % 4);
+    }
+
+    void RotateClockwise()
+    {
+        currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+    }
+
+    void MoveForward()
+    {
+        Vector3 moveDirection = Vector3.zero;
+        switch (currentDirection)
+        {
+            case Direction.Up:
+                moveDirection = Vector3.up;
+                break;
+            case Direction.Down:
+                moveDirection = Vector3.down;
+                break;
+            case Direction.Left:
+                moveDirection = Vector3.left;
+                break;
+            case Direction.Right:
+                moveDirection = Vector3.right;
+                break;
+        }
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+
+    void HandleRotationInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            RotateCounterClockwise();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            RotateClockwise();
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            float newSpeed = baseSpeed * 2;
+            speed = newSpeed;
+
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            speed = baseSpeed;
+        }
+
+
+    }
+    public void AddTail()
+    {
+        Vector3 newPosition = Vector3.zero;
+        switch (currentDirection)
+        {
+            case Direction.Up:
+                newPosition = tail[tail.Count - 1].transform.position - new Vector3(0, 1, 0);
+                break;
+            case Direction.Down:
+                newPosition = tail[tail.Count - 1].transform.position + new Vector3(0, 1, 0);
+                break;
+            case Direction.Left:
+                newPosition = tail[tail.Count - 1].transform.position + new Vector3(1, 0, 0);
+                break;
+            case Direction.Right:
+                newPosition = tail[tail.Count - 1].transform.position - new Vector3(1, 0, 0);
+                break;
+
+        }
+        GameObject newTail = Instantiate(tailPrefab, newPosition, Quaternion.identity);
+        tail.Add(newTail);
+    }
+    void UpdateTailPositions()
+    {
+        // A distância que cada segmento de cauda deve manter do segmento à sua frente. 
+        float segmentDistance = 1.2f;
+        for (int i = 1; i < tail.Count; i++)
+        {
+            GameObject currentSegment = tail[i];
+            GameObject segmentInFront = tail[i - 1];
+
+            // Calcula a nova posição como um ponto um pouco antes do segmento à frente. 
+            Vector3 newPosition = segmentInFront.transform.position -
+            (segmentInFront.transform.position - currentSegment.transform.position).normalized * segmentDistance;
+            currentSegment.transform.position = Vector3.Lerp(currentSegment.transform.position, newPosition, Time.deltaTime * 30);
+        }
+    }
+}
